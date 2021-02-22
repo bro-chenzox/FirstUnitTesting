5f86fe02c950e5defd6e37d23489d97239a3366f
Bro Chenzox <spalnavy@gmail.com>
2021-02-22 10:38:50 +0300

    Add RegistrationUtil.kt

---
+++ app/src/main/java/com/palchak/sergey/firstunittesting/RegistrationUtil.kt
@@ -0,0 +1,25 @@
+package com.palchak.sergey.firstunittesting
+
+object RegistrationUtil {
+
+    private val existingUsers = listOf("Peter", "Carl")
+
+    /**
+     * the input is not valid if...
+     *
+     * ...the userName/password is empty
+     *
+     * ...the userName is already taken
+     *
+     * ...the confirmed password is not the same as the real password
+     *
+     * ...the password contains less two digits
+     */
+    fun validateRegistrationInput(
+        userName: String,
+        password: String,
+        confirmedPassword: String
+    ): Boolean {
+        return true
+    }
+}
