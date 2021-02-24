1af8d383aa853dbe1c2bf588d5b4f42cd5e2c7a7
Bro Chenzox <spalnavy@gmail.com>
2021-02-24 08:58:22 +0300

    First Android test run

---
+++ app/src/androidTest/java/com/palchak/sergey/firstunittesting/ResourceComparerTest.kt
@@ -0,0 +1,32 @@
+package com.palchak.sergey.firstunittesting
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+
+import org.junit.Test
+
+class ResourceComparerTest {
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setUp() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "FirstUnitTesting")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "irstUnitTesting")
+        assertThat(result).isFalse()
+    }
+}
